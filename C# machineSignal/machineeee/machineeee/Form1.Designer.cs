b3eb21794d47e5229ce1e7e4439b968f8e46c606
geralddimaano <dlareg.onaamid@gmail.com>
2025-06-03 22:36:45 +0800

    Add files via upload

---
+++ C# machineSignal/machineeee/machineeee/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace machineeee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.green = new System.Windows.Forms.PictureBox();
+            this.yellow = new System.Windows.Forms.PictureBox();
+            this.red = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.start = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.inspect = new System.Windows.Forms.PictureBox();
+            this.package = new System.Windows.Forms.PictureBox();
+            this.assembly = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.q3 = new System.Windows.Forms.PictureBox();
+            this.q2 = new System.Windows.Forms.PictureBox();
+            this.q1 = new System.Windows.Forms.PictureBox();
+            this.q0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inspect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.package)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assembly)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.q3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::machineeee.Properties.Resources.ea9187918597ea29f65a1e45bbaeeddc_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 190);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 482);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // green
+            // 
+            this.green.Image = global::machineeee.Properties.Resources.green;
+            this.green.Location = new System.Drawing.Point(160, 106);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(198, 78);
+            this.green.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.green.TabIndex = 1;
+            this.green.TabStop = false;
+            // 
+            // yellow
+            // 
+            this.yellow.Image = global::machineeee.Properties.Resources.yellow;
+            this.yellow.Location = new System.Drawing.Point(160, 106);
+            this.yellow.Name = "yellow";
+            this.yellow.Size = new System.Drawing.Size(198, 78);
+            this.yellow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yellow.TabIndex = 2;
+            this.yellow.TabStop = false;
+            // 
+            // red
+            // 
+            this.red.Image = global::machineeee.Properties.Resources.red;
+            this.red.Location = new System.Drawing.Point(160, 106);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(198, 78);
+            this.red.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.red.TabIndex = 3;
+            this.red.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(201, 616);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 35);
+            this.start.TabIndex = 5;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // next
+            // 
+            this.next.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.next.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.Location = new System.Drawing.Point(128, 602);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(230, 49);
+            this.next.TabIndex = 6;
+            this.next.Text = "next station";
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // inspect
+            // 
+            this.inspect.Image = global::machineeee.Properties.Resources.e0cfd5f7bdc78fceb4d019ce261c1aa0;
+            this.inspect.Location = new System.Drawing.Point(40, 309);
+            this.inspect.Name = "inspect";
+            this.inspect.Size = new System.Drawing.Size(383, 221);
+            this.inspect.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.inspect.TabIndex = 7;
+            this.inspect.TabStop = false;
+            // 
+            // package
+            // 
+            this.package.Image = global::machineeee.Properties.Resources._8baa458b7ed0995a9351f9c0222b463b;
+            this.package.Location = new System.Drawing.Point(40, 309);
+            this.package.Name = "package";
+            this.package.Size = new System.Drawing.Size(383, 221);
+            this.package.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.package.TabIndex = 8;
+            this.package.TabStop = false;
+            // 
+            // assembly
+            // 
+            this.assembly.Image = global::machineeee.Properties.Resources.d377e2960eba57781c58eccbc892468c;
+            this.assembly.Location = new System.Drawing.Point(40, 309);
+            this.assembly.Name = "assembly";
+            this.assembly.Size = new System.Drawing.Size(383, 221);
+            this.assembly.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.assembly.TabIndex = 9;
+            this.assembly.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.q3);
+            this.panel1.Controls.Add(this.q2);
+            this.panel1.Controls.Add(this.q1);
+            this.panel1.Controls.Add(this.q0);
+            this.panel1.Location = new System.Drawing.Point(460, 286);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(688, 466);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // q3
+            // 
+            this.q3.Image = global::machineeee.Properties.Resources.Screenshot_2025_05_30_102551;
+            this.q3.Location = new System.Drawing.Point(3, 3);
+            this.q3.Name = "q3";
+            this.q3.Size = new System.Drawing.Size(682, 463);
+            this.q3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q3.TabIndex = 3;
+            this.q3.TabStop = false;
+            // 
+            // q2
+            // 
+            this.q2.Image = global::machineeee.Properties.Resources.Screenshot_2025_05_30_102652;
+            this.q2.Location = new System.Drawing.Point(3, 3);
+            this.q2.Name = "q2";
+            this.q2.Size = new System.Drawing.Size(682, 463);
+            this.q2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q2.TabIndex = 2;
+            this.q2.TabStop = false;
+            // 
+            // q1
+            // 
+            this.q1.Image = global::machineeee.Properties.Resources.Screenshot_2025_05_30_102519;
+            this.q1.Location = new System.Drawing.Point(3, 3);
+            this.q1.Name = "q1";
+            this.q1.Size = new System.Drawing.Size(682, 463);
+            this.q1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q1.TabIndex = 1;
+            this.q1.TabStop = false;
+            // 
+            // q0
+            // 
+            this.q0.Image = global::machineeee.Properties.Resources.Screenshot_2025_05_30_102457;
+            this.q0.Location = new System.Drawing.Point(3, 3);
+            this.q0.Name = "q0";
+            this.q0.Size = new System.Drawing.Size(682, 463);
+            this.q0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q0.TabIndex = 0;
+            this.q0.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::machineeee.Properties.Resources.Screenshot_2025_05_30_104259;
+            this.pictureBox2.Location = new System.Drawing.Point(1154, 420);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(696, 419);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::machineeee.Properties.Resources.S11creenshot_2025_05_25_140856;
+            this.pictureBox3.Location = new System.Drawing.Point(1154, 106);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(535, 308);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::machineeee.Properties.Resources.Screenshot_2025_05_25_142322;
+            this.pictureBox4.Location = new System.Drawing.Point(480, 85);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(648, 124);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::machineeee.Properties.Resources._5a8ae39ccba3d6e7eaaf5278b652c78d;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1838, 824);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.assembly);
+            this.Controls.Add(this.package);
+            this.Controls.Add(this.inspect);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.yellow);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inspect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.package)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assembly)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.q3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox green;
+        private System.Windows.Forms.PictureBox yellow;
+        private System.Windows.Forms.PictureBox red;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.PictureBox inspect;
+        private System.Windows.Forms.PictureBox package;
+        private System.Windows.Forms.PictureBox assembly;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox q0;
+        private System.Windows.Forms.PictureBox q3;
+        private System.Windows.Forms.PictureBox q2;
+        private System.Windows.Forms.PictureBox q1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ C# machineSignal/machineeee/machineeee/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace machineeee
+{
+    public partial class Form1 : Form
+    {
+        int change = 0;
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            green.Visible = false;
+            yellow.Visible = false;
+            red.Visible = false;
+            assembly.Visible = false;
+            inspect.Visible = false;
+            package.Visible = false;
+            next.Visible = false;
+            label1.Text = "            start the process";
+            q0.Visible = true;
+            q1.Visible = false;
+            q2.Visible = false;
+            q3.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            q0.Visible = false;
+            q1.Visible = true;
+            q2.Visible = false;
+            q3.Visible = false;
+            start.Visible = false;
+            green.Visible = true;
+            yellow.Visible = false;
+            red.Visible = false;
+            assembly.Visible =true;
+            inspect.Visible = false;
+            package.Visible = false;
+
+            label1.Text = "    The assembly station is operating";
+            timer1.Interval = 8000; 
+            change = 0;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (change == 5)
+            {
+                label1.Text = "        Well Done, can proceed back";
+                next.Visible = true;
+                next.Text = "back";
+                q0.Visible = false;
+                q1.Visible = false;
+                q2.Visible = false;
+                q3.Visible = true;
+                timer1.Stop();
+            }
+            else
+            {
+                label1.Text = "           proceed to next station";
+                next.Visible = true;
+                timer1.Stop();
+            }
+
+            
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+          if (next.Text == "back")
+            {
+                green.Visible = false;
+                yellow.Visible = false;
+                red.Visible = false;
+                assembly.Visible = false;
+                inspect.Visible = false;
+                package.Visible = false;
+                next.Visible = false;
+                label1.Text = "         start the process";
+                next.Text = "next station";
+                start.Visible = true;
+
+
+                timer1.Stop();
+                q0.Visible = true;
+                q1.Visible = false;
+                q2.Visible = false;
+                q3.Visible = false;
+            }
+      
+
+            if (change == 0)
+            {
+                label1.Text = "Items are being inspected in the inspection area";
+                change = 1;
+                green.Visible = false;
+                yellow.Visible = true;
+                red.Visible = false;
+                assembly.Visible = false;
+                next.Visible = false;
+                inspect.Visible = true;
+                package.Visible = false;
+                q0.Visible = false;
+                q1.Visible = false;
+                q2.Visible = true;
+                q3.Visible = false;
+            }
+            else if (change == 1) {
+
+                label1.Text = "Packaging is in progress in the packaging area";
+                change = 5;
+                green.Visible = false;
+                yellow.Visible = false;
+                red.Visible = true;
+                assembly.Visible = false;
+                inspect.Visible = false;
+                package.Visible = true;
+                next.Visible = false;
+                q1.Visible = true;
+                q2.Visible = false;
+              
+            }
+
+
+
+                timer1.Start();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
