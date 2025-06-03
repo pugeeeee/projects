027e8a7fd906355e78d847ce20a6111dd3a4f961
geralddimaano <dlareg.onaamid@gmail.com>
2025-06-03 22:39:08 +0800

    Add files via upload

---
+++ C# chatCommand/chat/chat/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chatBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.q3 = new System.Windows.Forms.PictureBox();
+            this.q2 = new System.Windows.Forms.PictureBox();
+            this.q1 = new System.Windows.Forms.PictureBox();
+            this.q0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.q3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.chatBox);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(44, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 820);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 734);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 738);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // chatBox
+            // 
+            this.chatBox.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.chatBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chatBox.Location = new System.Drawing.Point(3, 3);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(488, 672);
+            this.chatBox.TabIndex = 2;
+            this.chatBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(422, 681);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(3, 681);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(413, 38);
+            this.textBox2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::chat.Properties.Resources.Screenshot_2025_05_22_214809;
+            this.pictureBox1.Location = new System.Drawing.Point(544, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(712, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Controls.Add(this.q3);
+            this.panel2.Controls.Add(this.q2);
+            this.panel2.Controls.Add(this.q1);
+            this.panel2.Controls.Add(this.q0);
+            this.panel2.Location = new System.Drawing.Point(558, 364);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(709, 456);
+            this.panel2.TabIndex = 3;
+            // 
+            // q3
+            // 
+            this.q3.Image = global::chat.Properties.Resources.Screenshot_2025_05_29_184922;
+            this.q3.Location = new System.Drawing.Point(3, 3);
+            this.q3.Name = "q3";
+            this.q3.Size = new System.Drawing.Size(703, 442);
+            this.q3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q3.TabIndex = 3;
+            this.q3.TabStop = false;
+            // 
+            // q2
+            // 
+            this.q2.Image = global::chat.Properties.Resources.Screenshot_2025_05_29_184858;
+            this.q2.Location = new System.Drawing.Point(0, 3);
+            this.q2.Name = "q2";
+            this.q2.Size = new System.Drawing.Size(706, 442);
+            this.q2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q2.TabIndex = 2;
+            this.q2.TabStop = false;
+            // 
+            // q1
+            // 
+            this.q1.Image = global::chat.Properties.Resources.Screenshot_2025_05_29_184807;
+            this.q1.Location = new System.Drawing.Point(3, 3);
+            this.q1.Name = "q1";
+            this.q1.Size = new System.Drawing.Size(703, 445);
+            this.q1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q1.TabIndex = 1;
+            this.q1.TabStop = false;
+            // 
+            // q0
+            // 
+            this.q0.Image = global::chat.Properties.Resources.Screenshot_2025_05_29_184733;
+            this.q0.Location = new System.Drawing.Point(3, 3);
+            this.q0.Name = "q0";
+            this.q0.Size = new System.Drawing.Size(703, 442);
+            this.q0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.q0.TabIndex = 0;
+            this.q0.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::chat.Properties.Resources.Screenshot_2025_05_29_190716;
+            this.pictureBox2.Location = new System.Drawing.Point(1282, 435);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(586, 411);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::chat.Properties.Resources.Screenshot_2025_05_29_190104;
+            this.pictureBox3.Location = new System.Drawing.Point(1282, 65);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(559, 358);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(198, 145);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(205, 56);
+            this.start.TabIndex = 6;
+            this.start.Text = "open CHAT";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::chat.Properties.Resources._3f9cdcfce4bf50b7b163e9243b7330ca;
+            this.ClientSize = new System.Drawing.Size(1880, 875);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Commnad Chat";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.q3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.q0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox chatBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox q3;
+        private System.Windows.Forms.PictureBox q2;
+        private System.Windows.Forms.PictureBox q1;
+        private System.Windows.Forms.PictureBox q0;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C# chatCommand/chat/chat/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+
+        {
+            InitializeComponent();
+            chatBox.Text = "Bot : hello!" + Environment.NewLine + Environment.NewLine;
+            this.AcceptButton = button1;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            q0.Visible = true;
+            q1.Visible = false;
+            q2.Visible = false;
+            q3.Visible = false;
+            panel1.Visible = false;
+          
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            q1.Visible = false;
+
+
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            if (textBox2.Text == "/h")
+            {
+                q3.Visible = false;
+                q2.Visible = true;
+                chatBox.AppendText("You: /h" + Environment.NewLine);
+                chatBox.AppendText("Bot: Here are the list of commands that you can use:" + Environment.NewLine);
+                chatBox.AppendText(" /h (Help) - Show commands" + Environment.NewLine);
+                chatBox.AppendText(" /r (Reminder) - Sets a reminder for you" + Environment.NewLine);
+                chatBox.AppendText(" /i (Invite) - Generates invite link" + Environment.NewLine);
+                chatBox.AppendText(" /clear (Clear) - Clear history" + Environment.NewLine + Environment.NewLine);
+            }
+            else if (textBox2.Text == "/r")
+            {
+                q3.Visible = false;
+                q2.Visible = true;
+                chatBox.AppendText("You: /r" + Environment.NewLine);
+                chatBox.AppendText("Bot: Reminder set: Automata Presentation at 05/30/25" + Environment.NewLine + Environment.NewLine);
+            }
+            else if (textBox2.Text == "/clear")
+            {
+                q3.Visible = false;
+                q2.Visible = true;
+                chatBox.Clear();
+                chatBox.Text = "bot : hello!" + Environment.NewLine + Environment.NewLine;
+            }
+            else if (textBox2.Text == "/i")
+            {
+                q3.Visible = false;
+                q2.Visible = true;
+                chatBox.AppendText("You: /i" + Environment.NewLine);
+                chatBox.AppendText("Bot: https:208866998-Invites-101" + Environment.NewLine + Environment.NewLine);
+            }
+            else
+            {
+                q2.Visible = false;
+                q3.Visible = true;
+                chatBox.AppendText("You: " + textBox2.Text + Environment.NewLine);
+                chatBox.AppendText("Bot: Wrong command" + Environment.NewLine + Environment.NewLine);
+            }
+            textBox2.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            start.Visible = false;
+            q1.Visible = true;
+            q0.Visible=false;
+        }
+    }
+}
